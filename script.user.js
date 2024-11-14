5310e8e84cdf16f1c1181ca242d4a9004f92b9a5
Noah Kesterson <35641648+NoahBK@users.noreply.github.com>
2024-11-14 12:56:37 -0500

    Update script.user.js
    Further updated the script. Still can't get the icon on the left side of my name (your name if you are using this userscript) but I did fix the blurriness of the custom icon. I was using larger than 24x24 which was making the icon very blurry.

--- script.user.js
+++ script.user.js
@@ -1,61 +1,56 @@
 // ==UserScript==
-// @name         PayPalAvatarUnfucker
-// @namespace    https://violentmonkey.github.io/get-it/
-// @version      1.9
-// @homepage     https://github.com/NoahBK
-// @supportURL   https://github.com/NoahBK/PayPalAvatarUnfucker/issues
-// @downloadURL  https://github.com/NoahBK/PayPalAvatarUnfucker/raw/main/script.user.js
-// @updateURL    https://github.com/NoahBK/PayPalAvatarUnfucker/raw/main/script.user.js
-// @description  Replaces SVG with an image and removes the background color from PayPal's icon
-// @author       https://github.com/NoahBK
-// @match        https://www.paypal.com/*
-// @grant        none
+// @name        PayPalAvatarUnfucker
+// @author      NoahBK (https://github.com/NoahBK)  // Combined author and URL on one line
+// @namespace   https://violentmonkey.github.io/get-it/
+// @version     2.0
+// @homepage    https://github.com/NoahBK
+// @supportURL  https://github.com/NoahBK/PayPalAvatarUnfucker/issues
+// @downloadURL https://github.com/NoahBK/PayPalAvatarUnfucker/raw/main/script.user.js
+// @updateURL   https://github.com/NoahBK/PayPalAvatarUnfucker/raw/main/script.user.js
+// @description Replaces SVG with an image and removes the background color from PayPal's icon
+// @match       https://www.paypal.com/*
+// @grant       none
 // ==/UserScript==
+
 (function() {
-    'use strict';
+  'use strict';
 
-    const replaceIconWithImage = () => {
-        // Target the div containing the PayPal icon using the provided XPath
-        const iconDiv = document.evaluate('//*[@id="merchant-header-main-wrapper-internal"]/div/div[1]/div/div[2]/div[3]/div/div', document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+  const replaceIconWithImage = () => {
+    const iconDiv = document.evaluate('//*[@id="merchant-header-main-wrapper-internal"]/div/div[1]/div/div[2]/div[3]/div/div', document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
 
-        if (iconDiv) {
-            // Remove the background color, border, and padding that created the circle
-            iconDiv.style.backgroundColor = 'transparent';
-            iconDiv.style.border = 'none';
-            iconDiv.style.padding = '0';
-            iconDiv.style.borderRadius = '0'; // Remove any rounding on corners
+    if (iconDiv) {
+      iconDiv.style.backgroundColor = 'transparent';
+      iconDiv.style.border = 'none';
+      iconDiv.style.padding = '0';
+      iconDiv.style.borderRadius = '0';
+      iconDiv.style.width = '40px';
+      iconDiv.style.height = '40px';
 
-            // Find the SVG inside the div and remove it
-            const svgIcon = iconDiv.querySelector('svg');
-            if (svgIcon) {
-                svgIcon.remove();
-            }
+      const svgIcon = iconDiv.querySelector('svg');
+      if (svgIcon) {
+        svgIcon.remove();
+      }
 
-            // Add your image in place of the SVG
-            const img = document.createElement('img');
-            img.src = 'https://i.imgur.com/27aIpC1.png'; // Replace with your own image link
-            img.style.width = '40px';  // Set a fixed size to avoid stretching
-            img.style.height = '40px'; // Set height equal to width for consistency
-            img.style.objectFit = 'contain'; // Ensure the image is fully contained without stretching
-            img.style.borderRadius = '50%'; // Make the image circular
-            img.style.backgroundColor = 'transparent'; // Ensure no background color around the image
-            img.style.boxShadow = 'none'; // Remove any shadow around the image
-            img.style.maxWidth = '100%';
-            img.style.maxHeight = '100%';
+      const img = document.createElement('img');
+      img.src = 'https://i.imgur.com/27aIpC1.png';
+      img.style.width = '24px';
+      img.style.height = '24px';
+      img.style.objectFit = 'contain';
+      img.style.borderRadius = '50%';
+      img.style.backgroundColor = 'transparent';
+      img.style.boxShadow = 'none';
+      img.style.display = 'block';
+      img.style.margin = '0 auto';
+      iconDiv.appendChild(img);
 
-            // Insert the image inside the div
-            iconDiv.appendChild(img);
+      const redCircleDiv = document.evaluate('//*[@id="merchant-header-main-wrapper-internal"]/div/div[1]/div/div[2]/div[3]/div/div/div[1]', document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+      if (redCircleDiv) {
+        redCircleDiv.style.display = 'none';
+      }
+    } else {
+      setTimeout(replaceIconWithImage, 500);
+    }
+  };
 
-            // Remove the red circle element (if it still exists)
-            const redCircleDiv = document.evaluate('//*[@id="merchant-header-main-wrapper-internal"]/div/div[1]/div/div[2]/div[3]/div/div/div[1]', document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
-            if (redCircleDiv) {
-                redCircleDiv.style.display = 'none'; // Hide the red circle completely
-            }
-        } else {
-            // Retry if the div isn't found yet
-            setTimeout(replaceIconWithImage, 500);
-        }
-    };
-
-    replaceIconWithImage();
+  replaceIconWithImage();
 })();
